c4c00cbc2a36b59a9a003225b7300afcc266c58c
AndreySchmidt <ringnull@gmail.com>
2023-04-23 15:51:13 +0300

    cleaned up the garbage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="underline text-[48px] text-[orange]">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen flex justify-center items-center">
+      <h1 className="text-7x1 font-black">Spotify on React</h1>
     </div>
   );
 }
