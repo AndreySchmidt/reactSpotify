46798513a5c99036fee42f9942e90e28d11a92b8
AndreySchmidt <ringnull@gmail.com>
2023-04-28 15:53:05 +0300

    pl

--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -4,7 +4,16 @@ import PlaylistTitle from "./PlaylistTitle";
 import PlaylistDescription from "./PlaylistDescription";
 import PlaylistContextMenu from "./PlaylistContextMenu";
 
+import { useState } from "react";
+
 function Playlist({ coverUrl, title, description, classes }) {
+  const [isContextMenuOpen, setIsContextMenuOpen] = useState(false);
+
+  const openContextMenu = (event) => {
+    event.preventDefault();
+    setIsContextMenuOpen(true);
+  };
+
   const menuItems = [
     { label: "Add to Your Library" },
     {
@@ -20,6 +29,7 @@ function Playlist({ coverUrl, title, description, classes }) {
 
   return (
     <a
+      onContextMenu={openContextMenu}
       href="/"
       className={`relative p-4 rounded-md bg-[#181818] hover:bg-[#272727] duration-200 group ${classes}`}
     >
@@ -29,10 +39,13 @@ function Playlist({ coverUrl, title, description, classes }) {
       </div>
       <PlaylistTitle title={title} />
       <PlaylistDescription description={description} />
-      <PlaylistContextMenu
-        menuItems={menuItems}
-        classes="absolute top-9 left-9 bg-[#282828] text-[#eaeaea] text-sm divide-y divide-[#3e3e3e] p-1 rounded shadow-xl cursor-default whitespace-nowrap z-10 hidden group-hover:block"
-      />
+
+      {isContextMenuOpen && (
+        <PlaylistContextMenu
+          menuItems={menuItems}
+          classes="absolute top-9 left-9 bg-[#282828] text-[#eaeaea] text-sm divide-y divide-[#3e3e3e] p-1 rounded shadow-xl cursor-default whitespace-nowrap z-10"
+        />
+      )}
     </a>
   );
 }
