adaf187d8481b7153b2d27474b6efa1badf12419
AndreySchmidt <ringnull@gmail.com>
2023-05-25 19:50:36 +0300

    fix

--- src/hooks/useClickAway.js
+++ src/hooks/useClickAway.js
@@ -2,20 +2,14 @@ import { useEffect } from "react";
 
 function useClickAway(ref, handle, shouldHandle = () => true) {
   useEffect(() => {
-    // function handleClickAway({ target }) {
-    //   if (!ref.current.contains(target)) close();
-    // }
-
     function handleMousedown(event) {
       if (shouldHandle(event) && !ref.current.contains(event.target)) handle();
     }
     
     document.addEventListener("mousedown".handleMousedown);
-    document.addEventListener("mousedown", handleClickAway);
 
     return () => {
       document.removeEventListener("mousedown".handleMousedown);
-      document.removeEventListener("mousedown", handleClickAway);
     };
   });
 }
