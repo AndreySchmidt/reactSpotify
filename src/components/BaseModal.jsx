1aa94efbf250ac03ef83c6a1bb75e7bc05b6350f
AndreySchmidt <ringnull@gmail.com>
2023-05-27 11:09:50 +0300

    styles for TheModalEmbedPlaylist

--- src/components/BaseModal.jsx
+++ src/components/BaseModal.jsx
@@ -4,7 +4,7 @@ import { XMarkIcon } from "@heroicons/react/24/outline";
 import { useEffect, useRef } from "react";
 import useEvent from "../hooks/useEvent";
 
-function BaseModal({ onClose: handleClose, children }) {
+function BaseModal({ classes, onClose: handleClose, children }) {
   const ref = useRef();
   const contentRef = useRef();
 
@@ -30,13 +30,13 @@ function BaseModal({ onClose: handleClose, children }) {
 
   return ReactDOM.createPortal(
     <div
-      className="flex justify-center items-center fixed inset-0 bg-black/70 z-50 opacity-0 transition-opacity duration-500"
+      className={`flex justify-center items-center fixed inset-0 bg-black/70 z-50 opacity-0 transition-opacity duration-500`}
       role="dialog"
       onClick={close}
       ref={ref}
     >
       <div
-        className="flex flex-col bg-[#333] text-white h-80 w-[480px] rounded-xl relative -translate-y-10 transition-transform duration-500"
+        className={`flex flex-col text-white rounded-xl relative -translate-y-10 transition-transform duration-500 ${classes}`}
         onClick={(event) => event.stopPropagation()}
         ref={contentRef}
       >
--- src/components/TheModalEmbedPlaylist.jsx
+++ src/components/TheModalEmbedPlaylist.jsx
@@ -1,12 +1,22 @@
 import BaseModal from "./BaseModal";
+import BaseBtn from "./BaseBtn";
 
 function TheModalEmbedPlaylist({ onClose: handleClose }) {
   return (
-    <BaseModal onClose={handleClose}>
-      <h1 className="text-3xl pt-8 pb-3 px-8 font-bold leading-relaxed border-b border-neutral-600">
+    <BaseModal classes="w-[660px] bg-neutral-900" onClose={handleClose}>
+      <h1 className="text-3xl pt-8 pb-3 px-8 font-bold leading-relaxed">
         Embet
       </h1>
-      <div className="py-6 px-8 overflow-y-auto">Text for modal</div>
+      <div className="py-6 px-8 text-neutral-500 text-[13px]">
+        Text for
+        <a href="/" className="text-white font-bold hover:underline">
+          link
+        </a>
+        modal
+      </div>
+      <div className="text-right pb-6 px-8">
+        <BaseBtn>Copy</BaseBtn>
+      </div>
     </BaseModal>
   );
 }
